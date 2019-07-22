cb7606dee56339440e1a38b3e88e123e8d2d4ea0
Duran Hsieh <chhsie@microsoft.com>
2019-07-22 13:32:22 +0800

    init

---
+++ MVCFinalExample/Models/StudentModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCFinalExample.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext()
+        {
+            Database.SetInitializer<StudentDbContext>(null);
+        }
+
+        public DbSet<StudentProfile> StudentProfile { get; set; }
+    }
+
+    [Table("StudentProfile")]
+    public class StudentProfile
+    {
+        [Key]
+        public Guid guid { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Message { get; set; }
+    }
+}
